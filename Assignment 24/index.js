ac5732227527f249d21e151b890ebb473502040b
chandu1108 <4al21cg013@gmail.com>
2025-11-18 22:46:08 +0530

    Commit Done

---
+++ Assignment 24/index.js
@@ -0,0 +1,18 @@
+//Strings
+//single quotes
+console.log('Hello')
+//double quotes
+console.log("World")
+//back ticks
+let name="Chandan"
+console.log(`This is ${name}`)
+
+let name1="hari"
+let age=2.2
+let city="blr"
+let v='m'
+console.log(-1/0)
+
+let p=99999999999n
+let ph=BigInt(34567898765)
+console.log(typeof(ph))
